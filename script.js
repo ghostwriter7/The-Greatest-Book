7086e7b357a58cf3d5637777858647cacbb14c2b
ghostwriter7 <87965894+ghostwriter7@users.noreply.github.com>
2021-08-20 15:24:16 +0300

    Add sidebar showing other books of the author

--- script.js
+++ script.js
@@ -1,6 +1,32 @@
+// MOBILE NAV TOGGLE FUNCTIONALITY
+
 const toggle = document.getElementById('mobileToggle');
 const mobileNav = document.getElementById('mobile');
 
 toggle.addEventListener('click', () => {
-    mobileNav.classList.toggle('scrollDown');
+    mobileNav.classList.toggle('displayNav');
+})
+
+// NAVBAR - HEIGHT CHANGES WHEN USER SCROLLS DOWN 
+
+const navBar = document.querySelector('nav');
+
+window.addEventListener('scroll', () => {
+    if (window.scrollY !== 0 && window.innerWidth > 1150) {
+        navBar.classList.add('scrollDown');
+    } else if (window.scrollY === 0 && window.innerWidth > 1150) {
+        navBar.classList.remove('scrollDown');
+    }
+})
+
+// SIDE BAR WITH OTHER BOOKS 
+const linkOpen = document.getElementById('sideBarOpen')
+const closeSideBar = document.getElementById('closeSideBar');
+const sideBar = document.querySelector('.sideBarBox')
+
+linkOpen.addEventListener('click', () => {
+    sideBar.classList.add('active');
+})
+closeSideBar.addEventListener('click', () => {
+    sideBar.classList.remove('active');
 })
